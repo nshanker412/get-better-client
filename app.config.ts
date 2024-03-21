eb0a285f34e956034d524a0e020ada2cad3ed7e1
erictbenson10 <erictbenson10@gmail.com>
2024-03-20 17:10:31 -0700

    wip

--- app.config.ts
+++ app.config.ts
@@ -5,10 +5,10 @@ import { ConfigContext, ExpoConfig } from 'expo/config';
 
 export default ({ config }: ConfigContext): Partial<ExpoConfig> => {
 
-	console.log('process.env', process.env);
-	if (!process.env.SENTRY_AUTH_TOKEN) {
-		throw new Error('SENTRY_DSN and SENTRY_AUTH_TOKEN must be set');
-	}
+	// console.log('process.env', process.env);
+	// if (!process.env.SENTRY_AUTH_TOKEN) {
+	// 	throw new Error('SENTRY_DSN and SENTRY_AUTH_TOKEN must be set');
+	// }
 
 
 	return {
