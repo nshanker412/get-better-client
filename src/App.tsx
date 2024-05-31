a40dcbb43d5a942cedfe51d89bc1775cf6781a4a
erictbenson10 <erictbenson10@gmail.com>
2024-05-31 10:18:32 -0700

    remove expo

--- src/App.tsx
+++ src/App.tsx
@@ -1,36 +1,37 @@
 import { AuthProvider } from '@context/auth/AuthProvider';
 import { ThemeContextProvider } from '@context/theme/ThemeContextProvider';
 import { toastConfig } from '@context/theme/toastConfig';
-import * as Sentry from '@sentry/react-native';
+// import * as Sentry from '@sentry/react-native';
 import 'expo-dev-client';
 import React from 'react';
 import Toast from 'react-native-toast-message';
 
-const routingInstrumentation = new Sentry.ReactNavigationInstrumentation();
+// const routingInstrumentation = new Sentry.ReactNavigationInstrumentation();
 
-Sentry.init({
-	dsn: process.env.EXPO_PUBLIC_SENTRY_DSN,
-	enableAutoSessionTracking: true,
-	environment: process.env.EXPO_PUBLIC_ENVIRONMENT,
-	// debug: process.env.EXPO_PUBLIC_ENVIRONMENT !== 'production',
-	tracesSampleRate: 0.2,
-	integrations: [
-		new Sentry.ReactNativeTracing({
-			routingInstrumentation,
-		}),
-	],
-});
+// Sentry.init({
+// 	dsn: process.env.EXPO_PUBLIC_SENTRY_DSN,
+// 	enableAutoSessionTracking: true,
+// 	environment: process.env.EXPO_PUBLIC_ENVIRONMENT,
+// 	// debug: process.env.EXPO_PUBLIC_ENVIRONMENT !== 'production',
+// 	tracesSampleRate: 0.2,
+// 	integrations: [
+// 		new Sentry.ReactNativeTracing({
+// 			routingInstrumentation,
+// 		}),
+// 	],
+// });
 
 const App = () => {
 	return (
 		<>
 			<ThemeContextProvider>
-			<AuthProvider routingInstrumentation={routingInstrumentation} />
+			<AuthProvider routingInstrumentation={null} />
 			</ThemeContextProvider>
 		<Toast config={toastConfig} />
 		</>
 	);
 };
 
-export default Sentry.wrap(App);
+// export default Sentry.wrap(App);
+export default App;
 
