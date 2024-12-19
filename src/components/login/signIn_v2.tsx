927de0c298e8ef2c9d3a1d72464773364463dda5
kunal kumar <kunalkumar.kus@gmail.com>
2024-12-19 16:21:26 +0530

    update sign in color

--- src/components/login/signIn_v2.tsx
+++ src/components/login/signIn_v2.tsx
@@ -75,95 +75,95 @@ export const SignIn: React.FC = () => {
 		<SafeAreaView>
 			{/* <Header /> */}
 
-				<ImageBackground source={require("../../../assets/signin_background.png")} imageStyle={loginStyles.backgroundImage}  >
-					<View style={loginStyles.loginContainer}>
-						<View style={{ gap: 10 , marginBottom:70}}>
-							<Text style={loginStyles.SignInText}>SIGN IN</Text>
-							<Text
-								style={loginStyles.SignInSubtext}>
-								Improve everyday with everyone
-							</Text>
-						</View>
-						<View style={loginStyles.inputContainer}>
+			<ImageBackground source={require("../../../assets/signin_background.png")} imageStyle={loginStyles.backgroundImage}  >
+				<View style={loginStyles.loginContainer}>
+					<View style={{ gap: 10, marginBottom: 70 }}>
+						<Text style={loginStyles.SignInText}>SIGN IN</Text>
+						<Text
+							style={loginStyles.SignInSubtext}>
+							Improve everyday with everyone
+						</Text>
+					</View>
+					<View style={loginStyles.inputContainer}>
 						<View style={loginStyles.inputSubContainer}>
-						<Text style={loginStyles.lableText}>EMAIL ADDRESS</Text>
+							<Text style={loginStyles.lableText}>EMAIL ADDRESS</Text>
 
-						<SvgXml style={loginStyles.iconContainer} xml={MailIcon} color="#ff0000" size={20}/>
-						
-						<TextInput
-							style={[loginStyles.input,
+							<SvgXml style={loginStyles.iconContainer} xml={MailIcon} color="#ff0000" size={20} />
+
+							<TextInput
+								style={[loginStyles.input,
 								isFocused && loginStyles.inputFocused
-							]}
-							ref={emailRef}
-							onChangeText={setEmail}
-							autoCapitalize='none'
-							placeholderTextColor={theme.containerDefaultTextColor}
-							spellCheck={false}
-							autoCorrect={false}
-							keyboardAppearance='dark'
-							returnKeyType='next'
-							onSubmitEditing={() => {
-								passwordRef.current?.focus();
-							}}
-							onFocus={() => setIsFocused(true)}
-							onBlur={() => setIsFocused(false)}
+								]}
+								ref={emailRef}
+								onChangeText={setEmail}
+								autoCapitalize='none'
+								placeholderTextColor={theme.containerDefaultTextColor}
+								spellCheck={false}
+								autoCorrect={false}
+								keyboardAppearance='dark'
+								returnKeyType='next'
+								onSubmitEditing={() => {
+									passwordRef.current?.focus();
+								}}
+								onFocus={() => setIsFocused(true)}
+								onBlur={() => setIsFocused(false)}
 
-						/>
+							/>
 
 						</View>
 						<View style={loginStyles.inputSubContainer}>
-						<Text style={loginStyles.lableText}>PASSWORD</Text>
-						<SvgXml style={loginStyles.iconContainer} xml={LockIcon} color="#ff0000" size={20}/>
+							<Text style={loginStyles.lableText}>PASSWORD</Text>
+							<SvgXml style={loginStyles.iconContainer} xml={LockIcon} color="#ff0000" size={20} />
 
-						<TextInput
-							ref={passwordRef}
-							style={[loginStyles.input,
+							<TextInput
+								ref={passwordRef}
+								style={[loginStyles.input,
 								isPasswordFocused && loginStyles.inputFocused
-							]}
-							secureTextEntry={!showPassword}
-							onChangeText={setPassword}
-							autoCapitalize='none'
-							placeholderTextColor={theme.containerDefaultTextColor}
-							keyboardAppearance='dark'
-							returnKeyType='next'
-							onSubmitEditing={Keyboard.dismiss}
-							onFocus={() => setIsPasswordFocused(true)}
-							onBlur={() => setIsPasswordFocused(false)}
+								]}
+								secureTextEntry={!showPassword}
+								onChangeText={setPassword}
+								autoCapitalize='none'
+								placeholderTextColor={theme.containerDefaultTextColor}
+								keyboardAppearance='dark'
+								returnKeyType='next'
+								onSubmitEditing={Keyboard.dismiss}
+								onFocus={() => setIsPasswordFocused(true)}
+								onBlur={() => setIsPasswordFocused(false)}
 
-						/>
-						<TouchableOpacity 
-							style={loginStyles.eyeButton}
-							onPress={() => setShowPassword(!showPassword)}
-							>
-							<Icon
-								name={showPassword ? 'eye-outline' : 'eye-off-outline'}
-								size={24}
-								color="#666"
 							/>
+							<TouchableOpacity
+								style={loginStyles.eyeButton}
+								onPress={() => setShowPassword(!showPassword)}
+							>
+								<Icon
+									name={showPassword ? 'eye-outline' : 'eye-off-outline'}
+									size={24}
+									color="#666"
+								/>
 							</TouchableOpacity>
 						</View>
-						</View>
-						<View style={{ flexDirection: 'column', gap: 20, alignItems: "center" }}>
-							<TouchableOpacity
-								style={[
-									loginStyles.loginButton,
-									loading && {
-										opacity: 0.5,
-										backgroundColor: '',
-									},
-								]}
-								onPress={login}
-								disabled={loading}
-								activeOpacity={0.8}>
-								<View
-									style={{
-										flexDirection: 'row',
-										maxHeight: 45,
-										alignItems: 'center',
-										justifyContent: 'flex-end',
-										maxWidth: 200,
-									}}>
-										<>
+					</View>
+					<View style={{ flexDirection: 'column', gap: 20, alignItems: "center" }}>
+						<TouchableOpacity
+							style={[
+								loginStyles.loginButton,
+								loading && {
+									opacity: 0.5,
+									backgroundColor: '',
+								},
+							]}
+							onPress={login}
+							disabled={loading}
+							activeOpacity={0.8}>
+							<View
+								style={{
+									flexDirection: 'row',
+									maxHeight: 45,
+									alignItems: 'center',
+									justifyContent: 'flex-end',
+									maxWidth: 200,
+								}}>
+								<>
 									<Text
 										style={[
 											loginStyles.loginText,
@@ -173,33 +173,33 @@ export const SignIn: React.FC = () => {
 										SIGN IN
 
 									</Text>
-									<SvgXml style={loginStyles.iconSignInContainer} xml={ArrowIcon} color="#ff0000" size={20}/>
-									</>
-									<>
-										{loading && (
-											<View style={{ width: 20, height: 20 }}>
-												<LoadingSpinner />
-											</View>
-										)}
-									</>
-								</View>
-							</TouchableOpacity>
-							<TouchableOpacity onPress={onClickSignUp}>
-								<Text style={loginStyles.otherText}>
-									Don&apos;t have an account? &nbsp;
-									<Text  style={loginStyles.SignUp}>Sign Up.</Text>
-								</Text>
-							</TouchableOpacity>
-							<TouchableOpacity
-								style={loginStyles.forgotPasswordContainer}
-								onPress={onClickPasswordReset}>
-								<Text style={loginStyles.transferText}>
-									Forgot Password
-								</Text>
-							</TouchableOpacity>
-						</View>
+									<SvgXml style={loginStyles.iconSignInContainer} xml={ArrowIcon} color="#ff0000" size={20} />
+								</>
+								<>
+									{loading && (
+										<View style={{ width: 20, height: 20 }}>
+											<LoadingSpinner />
+										</View>
+									)}
+								</>
+							</View>
+						</TouchableOpacity>
+						<TouchableOpacity onPress={onClickSignUp}>
+							<Text style={loginStyles.otherText}>
+								Don&apos;t have an account? &nbsp;
+								<Text style={loginStyles.SignUp}>Sign Up.</Text>
+							</Text>
+						</TouchableOpacity>
+						<TouchableOpacity
+							style={loginStyles.forgotPasswordContainer}
+							onPress={onClickPasswordReset}>
+							<Text style={loginStyles.transferText}>
+								Forgot Password
+							</Text>
+						</TouchableOpacity>
 					</View>
-				</ImageBackground>
+				</View>
+			</ImageBackground>
 		</SafeAreaView>
 	);
 };
