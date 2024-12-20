4ecda8949bee35f052f6622ba810b4a4e9970549
kunal kumar <kunalkumar.kus@gmail.com>
2024-12-20 11:18:14 +0530

    update theme

--- src/components/challenge-modal/ChallengeModal.tsx
+++ src/components/challenge-modal/ChallengeModal.tsx
@@ -45,7 +45,7 @@ export const ChallengeModal: React.FC<{
       });
       setLoading(false);
       onClosePress();
-	  setChallengeText('');
+      setChallengeText('');
     } catch (err) {
       Toast.show({
         text1: `Hmm... that didn't go as planned.`,
@@ -59,92 +59,92 @@ export const ChallengeModal: React.FC<{
   };
 
   const onCloseModalPress = () => {
-	Keyboard.dismiss();
-	onClosePress();
-	setChallengeText('');
+    Keyboard.dismiss();
+    onClosePress();
+    setChallengeText('');
 
   }
 
   return (
     <Modal isVisible={isVisible} style={{ width: '90%', height: '80%' }}>
       <TouchableWithoutFeedback onPress={() => Keyboard.dismiss()} accessible={false}>
-	  <View style={{ flex: 1, alignContent: "stretch", justifyContent: "space-around" }}> 
+        <View style={{ flex: 1, alignContent: "stretch", justifyContent: "space-around" }}>
 
-        <Modal.Container
+          <Modal.Container
 
-          containerStyle={{
-            backgroundColor: theme.innerContainer.backgroundColor,
-            gap: 20,
-			flexDirection: 'column',
-            alignContent: 'center',
-            justifyContent: 'space-around',
-          }}>
-          <TouchableOpacity style={{alignItems: "flex-start", justifyContent: "flex-start", width: 40, height: 40}}onPress={onCloseModalPress}>
-            <View style={{ padding: 10}}>
-              <AntDesign name='close' size={24} color={theme.textColorPrimary} />
-            </View>
-          </TouchableOpacity>
-          <Modal.Header
+            containerStyle={{
+              backgroundColor: theme.innerContainer.backgroundColor,
+              gap: 20,
+              flexDirection: 'column',
+              alignContent: 'center',
+              justifyContent: 'space-around',
+            }}>
+            <TouchableOpacity style={{ alignItems: "flex-start", justifyContent: "flex-start", width: 40, height: 40 }} onPress={onCloseModalPress}>
+              <View style={{ padding: 10 }}>
+                <AntDesign name='close' size={24} color={theme.textColorPrimary} />
+              </View>
+            </TouchableOpacity>
+            <Modal.Header
 
-            title={`Do you want to challenge ${challengee}?`}
-            headerStyle={{
-              container: {
-                alignSelf: 'center',
-                justifyContent: 'center',
-              },
-              text: { ...theme.text.header },
-            }}
-          />
-          <Modal.Body bodyStyle={{ alignItems: 'center' }}>
-            <View style={{ gap: 0 , maxWidth: "90%", width: "90%"}}>
-			<Text style={[charactersUsed >= CHALLENGE_BODY_CHAR_LIMIT ? styles.maxCharReached : styles.charCount, { color: charactersUsed >= CHALLENGE_BODY_CHAR_LIMIT  ?  theme.errorColor : theme.grayShades.gray500, alignSelf: "flex-end", }]}>
-		    {charactersUsed}/{CHALLENGE_BODY_CHAR_LIMIT} 
-              </Text>
-              <KeyboardAvoidingView behavior={"padding"}>
-                <TextInput
-				        returnKeyType="done"
-
-                  style={{
-                    ...theme.innerContainer,
-                    borderWidth: 1,
-                    borderColor: theme.grayShades.gray400,
-                    borderRadius: 8,
-                    backgroundColor: theme.grayShades.gray300,
-                    width: '100%',
-					maxWidth: '100%',
-                    height: 100,
-                    // minWidth: '90%',
-                    textAlignVertical: 'top',
-                    padding: 10,
-                    color: theme.textColorPrimary,
-                    flexShrink: 0,
-                  }}
-                  placeholder='Enter instructions for your challenge'
-                  placeholderTextColor={theme.grayShades.gray500}
-				  multiline={true}
-				  maxLength={CHALLENGE_BODY_CHAR_LIMIT}
-				  keyboardType='twitter'
-				  keyboardAppearance='dark'
-                  value={challengeText}
-                  onChangeText={setChallengeText}
-				  onSubmitEditing={() => Keyboard.dismiss()} 
-                />
-              </KeyboardAvoidingView>
-            </View>
-          </Modal.Body>
-          <Modal.Footer>
-            <ActionButton
-              loading={loading}
-              isPrimary={true}
-              styles={{ container: { padding: 5,top:-14 } }}
-              defaultPressed={false}
-              title='Challenge'
-              onPress={onSubmitChallengeCb}
+              title={`Do you want to challenge ${challengee}?`}
+              headerStyle={{
+                container: {
+                  alignSelf: 'center',
+                  justifyContent: 'center',
+                },
+                text: { ...theme.text.header },
+              }}
             />
-            
-          </Modal.Footer>
-        </Modal.Container>
-		        </View>
+            <Modal.Body bodyStyle={{ alignItems: 'center' }}>
+              <View style={{ gap: 0, maxWidth: "90%", width: "90%" }}>
+                <Text style={[charactersUsed >= CHALLENGE_BODY_CHAR_LIMIT ? styles.maxCharReached : styles.charCount, { color: charactersUsed >= CHALLENGE_BODY_CHAR_LIMIT ? theme.errorColor : theme.grayShades.gray500, alignSelf: "flex-end", }]}>
+                  {charactersUsed}/{CHALLENGE_BODY_CHAR_LIMIT}
+                </Text>
+                <KeyboardAvoidingView behavior={"padding"}>
+                  <TextInput
+                    returnKeyType="done"
+
+                    style={{
+                      ...theme.innerContainer,
+                      borderWidth: 1,
+                      borderColor: theme.textColorSecondary,
+                      borderRadius: 8,
+                      backgroundColor: theme.grayShades.gray300,
+                      width: '100%',
+                      maxWidth: '100%',
+                      height: 100,
+                      // minWidth: '90%',
+                      textAlignVertical: 'top',
+                      padding: 10,
+                      color: theme.textColorPrimary,
+                      flexShrink: 0,
+                    }}
+                    placeholder='Enter instructions for your challenge'
+                    placeholderTextColor={theme.grayShades.gray500}
+                    multiline={true}
+                    maxLength={CHALLENGE_BODY_CHAR_LIMIT}
+                    keyboardType='twitter'
+                    keyboardAppearance='dark'
+                    value={challengeText}
+                    onChangeText={setChallengeText}
+                    onSubmitEditing={() => Keyboard.dismiss()}
+                  />
+                </KeyboardAvoidingView>
+              </View>
+            </Modal.Body>
+            <Modal.Footer>
+              <ActionButton
+                loading={loading}
+                isPrimary={true}
+                styles={{...theme.button.primary, container: { padding: 5, top: -14 }}}
+                defaultPressed={false}
+                title='Challenge'
+                onPress={onSubmitChallengeCb}
+              />
+
+            </Modal.Footer>
+          </Modal.Container>
+        </View>
 
       </TouchableWithoutFeedback>
     </Modal>
@@ -152,28 +152,28 @@ export const ChallengeModal: React.FC<{
 };
 
 const styles = StyleSheet.create({
-	// ... existing styles ...
-	charCount: {
-	
-	  fontSize: 12, // Smaller font size
-	  fontWeight: 'normal', // Less emphasis than the main header
-	  marginBottom: 4, // Space above the text input
-	  color: '#686868', // Slightly darker color, adjust based on your theme.grayShades.gray500
-	//   minWidth: 58,
-	// minWidth: 30,
-	  textAlign: "left",
-	},
-	charCountClose: {
-		fontSize: 12, // Smaller font size
-		fontWeight: 'normal', // Less emphasis than the main header
-		marginBottom: 4, // Space above the text input
-		color: '#686868', // Slightly darker color, adjust based on your theme.grayShades.gray500
-	  },
-	  maxCharReached: {
-		fontSize: 12, // Smaller font size
-		fontWeight: 'bold', // Less emphasis than the main header
-		marginBottom: 4, // Space above the text input
-		color: '#ff0000', // Slightly darker color, adjust based on your theme.grayShades.gray500
-	  },
+  // ... existing styles ...
+  charCount: {
 
-  });
+    fontSize: 12, // Smaller font size
+    fontWeight: 'normal', // Less emphasis than the main header
+    marginBottom: 4, // Space above the text input
+    color: '#C3FF45', // Slightly darker color, adjust based on your theme.grayShades.gray500
+    //   minWidth: 58,
+    // minWidth: 30,
+    textAlign: "left",
+  },
+  charCountClose: {
+    fontSize: 12, // Smaller font size
+    fontWeight: 'normal', // Less emphasis than the main header
+    marginBottom: 4, // Space above the text input
+    color: '#C3FF45', // Slightly darker color, adjust based on your theme.grayShades.gray500
+  },
+  maxCharReached: {
+    fontSize: 12, // Smaller font size
+    fontWeight: 'bold', // Less emphasis than the main header
+    marginBottom: 4, // Space above the text input
+    color: '#ff0000', // Slightly darker color, adjust based on your theme.grayShades.gray500
+  },
+
+});
